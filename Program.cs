40a928f60c905f34de9c9e38621a4ba1f3cb17de
peterwalcker <87015108+peterwalcker@users.noreply.github.com>
2024-01-10 17:37:51 +0300

    Lesson three

--- Program.cs
+++ Program.cs
@@ -7,4 +7,27 @@
             Console.WriteLine("Hello, World!");
         }
     }
+
+    #region task_7_3_3
+
+    abstract class ComputerPart
+    {
+        public abstract void Work();
+    }
+
+    class Processor : ComputerPart
+    {
+        public override void Work() { }
+    }
+    
+    class MotherBoard : ComputerPart
+    {
+        public override void Work() { }
+    }
+    
+    class GraphicCard : ComputerPart
+    {
+        public override void Work() { }
+    }
+    #endregion
 }
