50de778c8a8cfa79b9115384fba958266a418795
peterwalcker <87015108+peterwalcker@users.noreply.github.com>
2024-01-10 19:19:20 +0300

    Lesson six

--- Program.cs
+++ Program.cs
@@ -7,4 +7,54 @@
             Console.WriteLine("Hello, World!");
         }
     }
+
+    #region task_7_6_6
+    /// <summary>
+    /// task 7.6.2
+    /// </summary>
+    /// <typeparam name="T">ID</typeparam>
+    /// <typeparam name="K">Value</typeparam>
+    class Record<T, K>
+    {
+        public T Id;
+        public K Value;
+        public DateTime Date;
+    }
+    #endregion
+
+    #region task_7_6_2
+    /// <summary>
+    /// task 7.6.2
+    /// </summary>
+    abstract class Car<TEngine> where TEngine : Engine
+    {
+        public TEngine Engine;
+
+        public abstract void ChangePart<TPart>(TPart newPart) where TPart : Part;
+    }
+
+    class GasCar : Car<GasEngine>
+    {
+        public override void ChangePart<TPart>(TPart newPart) { }
+    }
+
+    class ElectricCar : Car<ElectricEngine>
+    {
+        public override void ChangePart<TPart>(TPart newPart) { }
+    }
+
+    abstract class Engine { }
+    
+    abstract class Part { }
+    
+    class GasEngine : Engine { }
+    
+    class ElectricEngine : Engine { }
+
+    class Battery : Part { }
+
+    class Differential : Part { }
+
+    class Wheel : Part { }
+    #endregion
 }
